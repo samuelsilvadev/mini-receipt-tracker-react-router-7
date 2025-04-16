16d3f698d44c86d7247729885403802c8df56b90
Samuel Silva <samuelsilvawb@gmail.com>
2025-04-16 22:42:53 +0100

    chore: add prettier + react router deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>,
-  )
+  );
 }
