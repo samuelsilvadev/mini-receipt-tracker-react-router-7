c9048e46cc71d3c8d2a5ea96f053c9cb1c568e60
Samuel Silva <samuelsilvawb@gmail.com>
2025-04-16 22:55:36 +0100

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Navigation } from "./components/Navigation";
+
 function App() {
-  return <div className="h-100"></div>;
+  return (
+    <div className="h-100 w-full">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router";
+
+const NAVIGATION = [
+  {
+    url: "/",
+    title: "Receipts",
+  },
+  {
+    url: "/add",
+    title: "Track Receipt",
+  },
+];
+
+export function Navigation() {
+  return (
+    <nav className="bg-white shadow-md w-full">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between h-16">
+          <span className="flex items-center ml-2 text-xl font-semibold text-gray-800">
+            ReceiptTracker
+          </span>
+
+          <ul id="navigation-list-mf" className="flex items-center space-x-8">
+            {NAVIGATION.map((item) => (
+              <li key={item.url}>
+                <NavLink
+                  to={item.url}
+                  className={({ isActive }) =>
+                    isActive ? "text-blue-800" : "text-black"
+                  }
+                >
+                  {item.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
